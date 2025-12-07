5132f9a3221bc6083916f439374ce62c1f38309f
Kumar Prakhar <kumarprakharkp143@gmail.com>
2025-12-07 17:01:33 +0530

    minor bugs fixed

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,12 +1,15 @@
 // src/api.js
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3000';
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000",
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
 
-export const googleAuth = (code) =>
-  api.get(`/api/auth/google?code=${code}`);
+export const googleAuth = async (code) => {
+  return api.get(`/api/auth/google?code=${code}`);
+}
 
 export default api;
